9ba0b923f3e80c983cc835b3a40a08aa669d85d9
Daz <devanshmahant1292003@gmail.com>
2026-09-14 15:24:42 +0530

    feat(platform): add trace explorer loading state

---
+++ apps/platform/app/projects/[projectId]/traces/loading.tsx
@@ -0,0 +1,20 @@
+import { AppShell } from '../../../../components/app-shell';
+import { Skeleton } from '../../../../components/ui/skeleton';
+
+export default function ProjectTracesLoading() {
+  return (
+    <AppShell>
+      <div className="grid gap-6">
+        <Skeleton className="h-5 w-64 rounded-xl" />
+        <Skeleton className="h-32 w-full rounded-xl" />
+        <div className="grid gap-4 sm:grid-cols-2 xl:grid-cols-4">
+          {Array.from({ length: 4 }).map((_, index) => (
+            <Skeleton key={index} className="h-28 rounded-xl" />
+          ))}
+        </div>
+        <Skeleton className="h-36 w-full rounded-xl" />
+        <Skeleton className="h-[420px] w-full rounded-xl" />
+      </div>
+    </AppShell>
+  );
+}
